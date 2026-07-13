0e30022a5ce2efc66ade8dccc02b818b2bb6c439
Paaventhan123 <paaventhanpkt@gmail.com>
2026-07-13 17:44:34 +0100

    S7-B: Scenario 7 Kiro Lambda hardcoded creds

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-# Scenario 6 - Condition B - Kiro - IAM Intentionally Misconfigured
+# Scenario 7 - Condition B - Kiro - Lambda Intentionally Misconfigured
 terraform {
   required_providers {
     aws = {
@@ -12,15 +12,15 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_iam_role" "s6_kiro_ec2" {
-  name_prefix = "kiro-s6-"
+resource "aws_iam_role" "s7_kiro_lambda" {
+  name = "kiro-s7-lambda-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Effect    = "Allow"
-        Principal = { Service = "ec2.amazonaws.com" }
+        Principal = { Service = "lambda.amazonaws.com" }
         Action    = "sts:AssumeRole"
       }
     ]
@@ -28,21 +28,33 @@ resource "aws_iam_role" "s6_kiro_ec2" {
 
   tags = {
     Project  = "dissertation"
-    Scenario = "S6-Kiro"
+    Scenario = "S7-Kiro"
   }
 }
 
-resource "aws_iam_role_policy_attachment" "s6_kiro_admin" {
-  role       = aws_iam_role.s6_kiro_ec2.name
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+resource "aws_iam_role_policy_attachment" "s7_kiro_lambda_basic" {
+  role       = aws_iam_role.s7_kiro_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-resource "aws_iam_instance_profile" "s6_kiro" {
-  name_prefix = "kiro-s6-profile-"
-  role = aws_iam_role.s6_kiro_ec2.name
+resource "aws_lambda_function" "s7_kiro" {
+  function_name = "kiro-s7-lambda"
+  role          = aws_iam_role.s7_kiro_lambda.arn
+  runtime       = "python3.11"
+  handler       = "lambda_function.lambda_handler"
+  filename      = "lambda_function.zip"
+
+  environment {
+    variables = {
+      APP_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE"
+      APP_SECRET_KEY = "wJalrXUtnFEMI12345KEY"
+      DB_PASSWORD    = "SuperSecret123!"
+      API_TOKEN      = "hardcoded-secret-token-12345"
+    }
+  }
 
   tags = {
     Project  = "dissertation"
-    Scenario = "S6-Kiro"
+    Scenario = "S7-Kiro"
   }
 }
